c4dae929fe5c305123c05408ef27070d2442d978
Emma Paris <artiverma@cedcoss.com>
2021-11-20 11:21:18 +0530

    this my third commit

--- app/src/main/java/com/example/myfirebaseapplication/MainActivity.kt
+++ app/src/main/java/com/example/myfirebaseapplication/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
 import com.google.android.gms.tasks.OnCompleteListener
 
 import com.google.firebase.FirebaseApp
@@ -13,14 +15,20 @@ import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.firestore.FirebaseFirestore
 
 import com.google.firebase.FirebaseOptions
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 
 
-
+lateinit var txtname:TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+         txtname = findViewById<TextView>(R.id.txtname)
 //        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
 //            if (!task.isSuccessful) {
 //                //Log.w(TAG, "Fetching FCM registration token failed", task.exception)
@@ -37,19 +45,38 @@ class MainActivity : AppCompatActivity() {
 //        })
        // FirebaseApp.initializeApp(this)
         GetSecondarydatabase(this)
-        val db = Firebase.firestore
-        val docRef = db.collection("User")
-        docRef.get()
-            .addOnSuccessListener { document ->
-                if (document != null) {
-                    Log.d("TAG", "DocumentSnapshot data: ${document.documents}")
-                } else {
-                    Log.d("TAG", "No such document")
-                }
+
+        val db = Firebase.database
+        val myRef = db.getReference("UserName")
+        //Log.d("TAG", "DocumentSnapshot data: ${myRef.get().}")
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.getValue<String>()
+                Log.d("TAG", "Value is: $value")
+                txtname.setText(value)
             }
-            .addOnFailureListener { exception ->
-                Log.d("TAG", "get failed with ", exception)
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w("TAG", "Failed to read value.", error.toException())
             }
+        })
+
+//        val docRef = db.collection("User").document("zb6XbRq7EWwTmgbeDhXI")
+//        docRef.get()
+//            .addOnSuccessListener { document ->
+//                if (document != null) {
+//                    Log.d("TAG", "DocumentSnapshot data: ${document.data?.values}")
+//                    txtname.setText(document.data?.values.toString())
+//                } else {
+//                    Log.d("TAG", "No such document")
+//                }
+//            }
+//            .addOnFailureListener { exception ->
+//                Log.d("TAG", "get failed with ", exception)
+//            }
    }
     fun GetSecondarydatabase(context: Context): FirebaseFirestore? {
         //  FirebaseApp app = FirebaseApp.initializeApp(context);
@@ -59,7 +86,7 @@ class MainActivity : AppCompatActivity() {
             val options = FirebaseOptions.Builder().setProjectId("checkkotlinproject")
                 .setApplicationId("contactcentral-61fa8")
                 .setApiKey("AIzaSyBH1SaHZ0sv-7NvUl9QRWk7eyciYBKjyUc")
-                .setDatabaseUrl("https://contactcentral-61fa8-default-rtdb.firebaseio.com")
+                .setDatabaseUrl("https://checkkotlinproject-default-rtdb.firebaseio.com")
                 .setStorageBucket("checkkotlinproject.appspot.com").build()
             app = FirebaseApp.initializeApp(context, options, "ContactFirebasePlugin")
             database = FirebaseFirestore.getInstance(app)
